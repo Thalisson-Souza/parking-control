6ecec8e988cdad00c692cb3f1ca12de985aaf4fa
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-02-14 01:57:05 -0300

    refactor: structure and relationships

--- src/main/java/com/api/parking_control/entity/Car.java
+++ src/main/java/com/api/parking_control/entity/Car.java
@@ -1,35 +1,52 @@
 package com.api.parking_control.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Entity
-@Table(name = "car")
+@Table(name = "TB_CAR")
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class Car {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
-    @Column(name = "responsibleCar")
-    @NotBlank
-    private String responsibleCar;
-
-    @Column(name = "plateCar")
-    @NotBlank
+    @Column(name = "plateCar", nullable = false)
+    @NotNull
     private String plateCar;
 
-    @Column(name = "modelCar")
-    @NotBlank
+    @Column(name = "modelCar",nullable = false)
+    @NotNull
     private String modelCar;
 
-    @Column(name = "colorCar")
-    @NotBlank
+    @Column(name = "colorCar",nullable = false)
+    @NotNull
     private String colorCar;
+
+
+    public Car() {}
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setPlateCar(String plateCar) {
+        this.plateCar = plateCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public void setColorCar(String colorCar) {
+        this.colorCar = colorCar;
+    }
 }
--- src/main/java/com/api/parking_control/entity/ParkingSpot.java
+++ src/main/java/com/api/parking_control/entity/ParkingSpot.java
@@ -1,44 +1,79 @@
 package com.api.parking_control.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "parking-spot")
+@Table(name = "TB_PARKING_SPOT")
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class ParkingSpot {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(name = "parkingSpotNumber", unique = true)
-    @NotBlank
+    @Column(name = "parkingSpotNumber", unique = true, nullable = false)
+    @NotNull
     private String parkingSpotNumber;
 
-    @Column(name = "responsibleName", unique = true)
-    @NotBlank
-    private String responsibleName;
-
-    @Column(name = "apartment/block", unique = true)
-    @NotBlank
-    private String block;
+    @OneToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "id", nullable = false)
+    @NotNull
+    private Car car;
 
     @Column(name = "date_register")
-    @NotBlank
     private LocalDateTime registrationDate;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "car_id", referencedColumnName = "id")
-    private Car car;
+    @Getter
+    @Column(name = "responsibleName", unique = true,nullable = false)
+    @NotNull
+    private String responsibleName;
+
+    @Getter
+    @Column(name = "apartment", unique = true,nullable = false)
+    @NotNull
+    private String block;
+
+
+
+
+    public ParkingSpot() {
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public void setResponsibleName(String responsibleName) {
+        this.responsibleName = responsibleName;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
 }
