8cf2df0f0df810172e91acba5a57d5c0b5b965ce
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-02-14 02:04:32 -0300

    feat: create parking spot

--- src/main/java/com/api/parking_control/services/ParkingSpotService.java
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -1,15 +1,30 @@
 package com.api.parking_control.services;
 
+import com.api.parking_control.dtos.ParkingSpotDTO;
+import com.api.parking_control.entity.Car;
+import com.api.parking_control.entity.ParkingSpot;
+import com.api.parking_control.repository.CarRepository;
 import com.api.parking_control.repository.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
 
     ParkingSpotRepository parkingSpotRepository;
-    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
-        this.parkingSpotRepository = parkingSpotRepository;
-    }
-    
+    private final CarRepository carRepository;
 
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository,
+                              CarRepository carRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+        this.carRepository = carRepository;
+    }
+
+    @Transactional
+    public ParkingSpot createParkingSpot(ParkingSpot parkingSpot){
+        return parkingSpotRepository.save(parkingSpot);
+    }
 }
