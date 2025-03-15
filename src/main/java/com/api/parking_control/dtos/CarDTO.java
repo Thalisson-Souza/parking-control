e77ec29b404bd84803ad0b821746a50d46bf1f3a
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-03-15 01:28:18 -0300

    style: Update code aesthetics

--- src/main/java/com/api/parking_control/dtos/CarDTO.java
+++ src/main/java/com/api/parking_control/dtos/CarDTO.java
@@ -3,11 +3,8 @@ package com.api.parking_control.dtos;
 import jakarta.validation.constraints.NotBlank;
 
 public record CarDTO(
-    @NotBlank
-    String plateCar,
-    @NotBlank
-    String modelCar,
-    @NotBlank
-    String colorCar
+    @NotBlank String plateCar,
+    @NotBlank String modelCar,
+    @NotBlank String colorCar
     )
 {}
